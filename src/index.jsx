30d2af8820b68c50f9838d102943e6454974e018
JoaoLeite47 <StalkerJJ@outlook.com>
2022-05-27 17:51:44 -0300

    inicial commit

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Home from "./components/Home.jsx";
+
+ReactDOM.render(
+  <React.StrictMode> <Home/> </React.StrictMode>,
+  document.getElementById("root")
+);
